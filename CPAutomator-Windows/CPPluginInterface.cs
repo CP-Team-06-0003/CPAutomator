f7204a2c158e0376a55c61dfeb8b9a611fc679aa
Josh Max <joshumax@hotmail.com>
2015-06-16 20:01:21 -0700

    Added command queue stubs to Plugin Interface

--- CPAutomator-Windows/CPPluginInterface.cs
+++ CPAutomator-Windows/CPPluginInterface.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace CPPlugin
 {
+    /* OS Type this plugin supports */
     public enum OS_TYPE
     {
         OS_WINDOWS,
         OS_LINUX
+    }
+
+    /* Queue of native shell commands */
+    public class CPCommandQueue
+    {
+        private IList<string> commands;
+
+        /// <summary>
+        /// Adds a native command to the Command Queue
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns>
+        /// Index of previous command
+        /// </returns>
+        public int addCommand(string cmd)
+        {
+            commands.Add(cmd);
+            return commands.Count - 2; // Return index of last command
+        }
+
+        /// <summary>
+        /// Adds a list of native commands to the Command Queue
+        /// </summary>
+        /// <param name="cmds"></param>
+        /// <returns>
+        /// Index of previous command
+        /// </returns>
+        public int addCommand(ICollection<string> cmds)
+        {
+            int ret = commands.Count - 1; // Index of last command
+            foreach (string s in cmds)
+                commands.Add(s);
+            return ret;
+        }
+
+        /* Removes a command at the specified index */
+        public void removeCommand(int index)
+        {
+            commands.RemoveAt(index);
+        }
+
+        public void clearCommands()
+        {
+            commands.Clear();
+        }
+
+        public IList<string> getCommands()
+        {
+            return commands;
+        }
     }
 
     public interface CPPluginInterface
--- CPAutomator-Windows/CPUniversalInterface.cs
+++ CPAutomator-Windows/CPUniversalInterface.cs
@@ -9,7 +9,11 @@ namespace CPAutomatorInterface
 {
     public interface CPUniversalInterface
     {
+        /* Gets a plugin handle by name */
         Assembly getPluginByName(string name);
+        /* Runs a native comand queue */
+        bool runCommandQueue(CPPlugin.CPCommandQueue q);
+        /* Log text to the automator */
         void Log(string text);
     }
 }
